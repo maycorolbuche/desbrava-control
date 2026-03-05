2439cb72907e1d5cb9d82dcff90229b8db3a5f3c
maycorolbuche <mayco_rolbuche@hotmail.com>
2026-03-05 20:33:11 -0300

    Ajuste deploy

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
-    base: env.VITE_BASE_URL,
+    base: './',
 
     plugins: [vue(), vueDevTools()],
 
