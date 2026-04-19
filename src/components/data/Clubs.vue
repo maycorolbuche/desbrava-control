d338311f19fb75c01497715b388af5b6940b4ecb
maycorolbuche <mayco_rolbuche@hotmail.com>
2026-04-19 15:07:36 -0300

    Filtros Clubes

--- src/components/data/Clubs.vue
+++ src/components/data/Clubs.vue
@@ -1,24 +1,21 @@
 <template>
   <v-card flat>
-    <v-toolbar color="transparent">
-      <v-toolbar-title text="Lista de Clubes"></v-toolbar-title>
-
-      <!-- TODO: Implantar recursos -->
-      <!--
-      <template v-slot:append>
-        <v-btn icon="mdi-sort"></v-btn>
-        <v-btn icon="mdi-filter-settings-outline"></v-btn>
-        <v-btn icon="mdi-magnify"></v-btn>
-      </template>
-      -->
-    </v-toolbar>
+    <Toolbar
+      title="Lista de Clubes"
+      searchable
+      :sortable="['asc', 'desc']"
+      :filterable="filterable"
+      v-model:search="search"
+      v-model:sort="sort"
+      v-model:filter="filter"
+    />
 
     <v-divider />
 
     <v-list>
       <v-skeleton-loader :loading="loading" type="list-item-two-line">
         <v-list-item
-          v-for="item in data"
+          v-for="item in filteredData"
           :key="item.id"
           :base-color="deleting.includes(item.id) ? 'error' : ''"
           :class="{ blur: deleting.includes(item.id) }"
@@ -64,19 +61,78 @@
 </template>
 
 <script setup>
-import { ref, toRef, onMounted } from 'vue'
+import { ref, toRef, onMounted, watch, computed } from 'vue'
 
 import ClubsForm from '@/components/data/ClubsForm.vue'
 import Alert from '@/helpers/Alert'
 import Api from '@/services/Api'
 import Dialog from '@/helpers/Dialog'
+import Toolbar from '@/components/Toolbar.vue'
+import Session from '@/helpers/Session'
 
 const loading = ref(false)
 const deleting = toRef([])
 const deleted = toRef([])
 const updating = toRef({})
-const data = toRef({})
+const data = toRef([])
 const sheet = ref(false)
+
+const search = ref(null)
+const sort = ref(null)
+const filter = ref(null)
+
+const filteredData = computed(() => {
+  let result = [...data.value] // Cria uma cópia do array original
+
+  // Aplica o filtro de busca (se existir)
+  if (search.value) {
+    result = result.filter((item) => item.name.toLowerCase().includes(search.value.toLowerCase()))
+  }
+
+  // Aplica o filtro por district (se existir)
+  if (filter.value && filter.value.district && filter.value.district.length > 0) {
+    result = result.filter(
+      (item) => item.district && filter.value.district.includes(item.district.id),
+    )
+  }
+
+  // Aplica a ordenação (se existir)
+  if (sort.value) {
+    if (sort.value === 'asc') {
+      result.sort((a, b) => a.name.localeCompare(b.name))
+    } else if (sort.value === 'desc') {
+      result.sort((a, b) => b.name.localeCompare(a.name))
+    }
+  }
+
+  return result
+})
+
+const filterable = computed(() => {
+  const filters = [{ key: 'district', title: 'Distrito', items: districtsData.value }]
+  return filters
+})
+
+const districtsData = computed(() => {
+  if (!data.value || !data.value.length) return []
+
+  // Pega todos os districts distintos
+  const districtsMap = new Map()
+
+  data.value.forEach((item) => {
+    if (item.district && item.district.id) {
+      if (!districtsMap.has(item.district.id)) {
+        districtsMap.set(item.district.id, {
+          id: item.district.id,
+          name: item.district.name,
+        })
+      }
+    }
+  })
+
+  // Converte o Map para array e ordena por nível ou nome
+  return Array.from(districtsMap.values()).sort((a, b) => b.level - a.level)
+})
 
 async function loadData() {
   loading.value = true
@@ -125,7 +181,30 @@ function saved(payload) {
   sheet.value = false
 }
 
+watch(
+  () => search.value,
+  () => {
+    Session.set('list-clubs-search', search.value)
+  },
+)
+watch(
+  () => sort.value,
+  () => {
+    Session.set('list-clubs-sort', sort.value)
+  },
+)
+watch(
+  () => filter.value,
+  () => {
+    Session.set('list-clubs-filter', filter.value)
+  },
+)
+
 onMounted(() => {
   loadData()
+
+  search.value = Session.get('list-clubs-search', null)
+  sort.value = Session.get('list-clubs-sort', null)
+  filter.value = Session.get('list-clubs-filter', null)
 })
 </script>
